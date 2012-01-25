f4905502eec1672c74a4eac83547c01e44cd8c60
C.J. Adams-Collier <cjac@colliertech.org>
2012-01-25 23:52:21 +0000

    Import of CJCOLLIER/NIST-NVD-0.05 from CPAN.
    gitpan-cpan-distribution: NIST-NVD
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         CJCOLLIER/NIST-NVD-0.05.tar.gz
    gitpan-cpan-author:       CJCOLLIER
    gitpan-cpan-maturity:     released

--- lib/NIST/NVD.pm
+++ lib/NIST/NVD.pm
@@ -9,11 +9,11 @@ NIST::NVD - Placeholder for general NIST::NVD utilities
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 
 =head1 SYNOPSIS
---
+++ lib/NIST/NVD/Store/Base.pm
@@ -0,0 +1,28 @@
+package NIST::NVD::Store::Base;
+
+use warnings;
+use strict;
+
+our $VERSION = '0.05';
+
+
+
+
+=head2 get_cve_for_cpe
+
+=cut
+
+sub get_cve_for_cpe {
+
+}
+
+=head2 get_cve
+
+
+=cut
+
+sub get_cve {
+
+}
+
+1;
